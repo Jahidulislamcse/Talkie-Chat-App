b6a80dfcffdd0397b01654bb98d38b3b98d73c0b
unknown <mislam181266@bscse.uiu.ac.bd>
2023-10-13 00:15:46 +0600

    Inital Server side initiated with importing necessary libraries and resolving some cors issues with the socket.io

---
+++ server/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io"); //a class from socket.io library
+
+app.use(cors());
+
+const server = http.createServer(app);
+const io = new Server(server, {
+  /*instantiate Server using io to say I need to connect socket.io 
+    with express I created, object to solve cors issues consisting some 
+    credential or settings related to cors in socket.io server */
+  cors: {
+    origin: "http://localhost:3000",
+    /*To tell socket.io that it is okay to accept socket communication with this url ,
+        Telling this url the server which url/server is gonna be calling
+         to socket.io server*/
+    methods: ["GET", "POST", "PUT"],
+  },
+});
+
+server.listen(3001, () => {
+  console.log("Server is Running");
+});
